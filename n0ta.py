78ce60a33a3300954c75f2a11d3e7b2477d53f43
Th1iago3 <140936706+Th1iago3@users.noreply.github.com>
2024-08-07 14:59:39 -0300

    Update n0ta.py

--- n0ta.py
+++ n0ta.py
@@ -1,208 +1,141 @@
-import os
-import re
-import time
-import shutil
-import platform
-import sys
-import uuid
-import requests
 import json
+import os
+import random
+import string
+import datetime
+import subprocess
+import signal
+import time
 
-# \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+# Função para gerar um ID aleatório
+def generate_id():
+    return ''.join(random.choices(string.ascii_letters + string.digits, k=10))
 
-plist_path = "/rootfs/var/mobile/Library/Application Support/Flex3/patches.plist"
-txt_path = "/rootfs/var/mobile/Library/Application Support/Flex3/patches.txt"
-verbose_folder = "/var/mobile/N0tA/_verboseIndex/"
-info_file = os.path.join(verbose_folder, "_infoplist")
-read_puaf_file = os.path.join(verbose_folder, "_readPuafPages")
-status_puaf_file = os.path.join(verbose_folder, "_statusPuaf")
-apps_bundles_file = os.path.join(verbose_folder, "_AppsBundles")
-_installedx = "/var/mobile/N0tA/_installed.jjK"
+# Função para criar um novo arquivo JSON com os valores iniciais
+def create_json_file(directory):
+    id = generate_id()
+    timestamp = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+    filename = f"{id}-{timestamp}.json"
+    filepath = os.path.join(directory, filename)
+    
+    data = {
+        "app_version": "{dopamine_or_roothide}",
+        "timestamp": timestamp,
+        "- (bool)app_state": "false"
+    }
+    
+    with open(filepath, 'w') as json_file:
+        json.dump(data, json_file)
+    
+    return filepath
 
+# Função para ler o arquivo JSON
+def read_json_file(filepath):
+    with open(filepath, 'r') as json_file:
+        return json.load(json_file)
 
-# \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+# Função para escrever no arquivo JSON
+def write_json_file(filepath, data):
+    with open(filepath, 'w') as json_file:
+        json.dump(data, json_file)
 
-if not os.path.exists(verbose_folder):
-    os.makedirs(verbose_folder)
-    with open(info_file, 'w'): pass
-    with open(read_puaf_file, 'w'): pass
-    with open(status_puaf_file, 'w'): pass
-    with open(apps_bundles_file, 'w'): pass
-
-
-# \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
-
-apps_bundles = {}
-if os.path.exists(apps_bundles_file):
-    with open(apps_bundles_file, 'r') as file:
-        for line in file:
-            bundle, app_name = line.strip().split('=')
-            apps_bundles[bundle] = app_name
-
-
-# \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
-
-default_bundles = {
-    "net.whatsapp.WhatsApp": "WhatsApp",
-    "ph.telegra.Telegraph": "Telegram",
-    "com.google.ios.youtube": "YouTube",
-    "net.whatsapp.WhatsAppSMB": "WA Business",
-    "com.tigisoftware.Filza": "Filza",
-    "com.burbn.instagram": "Instagram"
-}
-
-for bundle, app_name in default_bundles.items():
-    if bundle not in apps_bundles:
-        with open(apps_bundles_file, 'a') as file:
-            file.write(f"{bundle}={app_name}\n")
-
-        apps_bundles[bundle] = app_name
-
-
-# \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
-
-def clear():
-    os.system("clear")
-
-# \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
-
-def clean_tag(text):
-    return re.sub(r'<[^>]*>', '', text)
-
-# \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
-
-def update_apps_bundles(bundle_id, app_name):
-    with open(apps_bundles_file, 'a') as file:
-        file.write(f"{bundle_id}={app_name}\n")
-    apps_bundles[bundle_id] = app_name
-
-# \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
-
-def credentials_user_uuid():
-    return str(uuid.uuid4())
-
-# \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
-
-def find_app(bundle_id):
-    if bundle_id in apps_bundles:
-        return apps_bundles[bundle_id]
+# Função para atualizar o conteúdo do arquivo .sh
+def update_sh_file(app_choice, active):
+    sh_file = f"{app_choice}_starter.sh"
+    if active:
+        command = f"nohup python3 ./{app_choice}_ref.py -s > ./{app_choice}_LogCat.out 2>&1 &"
     else:
-        res = requests.get(f"https://itunes.apple.com/lookup?bundleId={bundle_id}&lang=pt-BR")
-        if res.status_code == 200:
-           data = json.loads(res.content)
-           if data["results"]:
-            app_name = data["results"][0]["trackName"]
-           else:
-            print(f"[ {time.strftime('%Y-%m-%d %H:%M:%S')} ]: '{bundle_id}' not found on LibHook")
-        else:
-            raise ValueError(f"[ {time.strftime('%Y-%m-%d %H:%M:%S')} ]: '{bundle_id}' error on puafA_read: {res.status_code}")
+        command = f"nohup python3 ./{app_choice}_ref.py > ./{app_choice}_LogCat.out 2>&1 &"
+        # Remove the log file
+        log_file = f"./{app_choice}_LogCat.out"
+        if os.path.exists(log_file):
+            os.remove(log_file)
+    
+    with open(sh_file, 'w') as file:
+        file.write(f"#!/bin/sh\n{command}\n")
+    os.chmod(sh_file, 0o755)  # Torna o arquivo .sh executável
 
 
-        if app_name:
-            with open(apps_bundles_file, 'a') as file:
-                file.write(f"{bundle_id}={app_name}\n")
-            apps_bundles[bundle_id] = app_name
 
-            # Log
-            print(f"[ {time.strftime('%Y-%m-%d %H:%M:%S')} ]: '{bundle_id}' is saved on libhook.")
+# Função para verificar se os arquivos estão instalados
+def check_installation():
+    required_files = [
+        "./_verboseIndex/",
+        "./global_ref.py",
+        "./roothide_ref.py",
+        "alljailB_starter.sh",
+        "roothide_starter.sh",
+        "allappstate.sh"
+    ]
+    
+    for file in required_files:
+        if not os.path.exists(file):
+            print(f"{file} não encontrado.")
+            break
 
-        return app_name
+def checod():
+    required_files = [
+        "./_installed.jjK"
+    ]
+    
+    for file in required_files:
+        if not os.path.exists(file):
+            print(f"{file} não encontrado. Instalando Módulos...")
+            os.system("sh _install.sh")
+            break
 
-# \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+# Função principal
+def main():
+    check_installation()
+    checod()
 
-def refresh_app(app_name):
-    os.system(f"killall {app_name} 2>/dev/null")
+    directory = "./_verboseIndex/"
+    if not os.path.exists(directory):
+        os.makedirs(directory)
 
-# \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
-
-def log(message):
-    print(f"[+] {message}")
-
-
-# \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
-
-def x():
-    if not os.path.exists(_installedx):
-        clear()
-        log("_installed.jjK not installed - installing modules... ")
-        os.system("sh _install.sh")
-        return
-
-
-# \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
-clear()
-x()
-if len(sys.argv) < 2:
-    print("\n\n\n[+] Use: [ -r (readOnly) | -s (start) ]\n       [+]: [ -i (info) ]")
-    sys.exit()
-
-if sys.argv[1] == "-i":
-   credn = credentials_user_uuid()
-   iOS = platform.machine()
-   vrs = platform.version()
-   print(f"\n\n\n[ {time.strftime('%Y-%m-%d %H:%M:%S')} ]: 0xfffffff00a4734f0")
-   print(f"[ {time.strftime('%Y-%m-%d %H:%M:%S')} ]: Machine: {iOS}")
-   print(f"[ {time.strftime('%Y-%m-%d %H:%M:%S')} ]: kernel_version: {vrs}")
-   print(f"[ {time.strftime('%Y-%m-%d %H:%M:%S')} ]: script_version: Bootstrap ")
-   sys.exit()
-
-if sys.argv[1] == "-r":
-    iOS = platform.machine()
-    vrs = platform.version()
-    print(f"\n\n\n[ {time.strftime('%Y-%m-%d %H:%M:%S')} ]: Machine: {iOS}")
-    print(f"[ {time.strftime('%Y-%m-%d %H:%M:%S')} ]: kernel_version: {vrs}")
-    with open(plist_path, 'a') as file:
-            print(f"[ {time.strftime('%Y-%m-%d %H:%M:%S')} ]: {plist_path} :200: open\n\n")
-    sys.exit()
-
-if sys.argv[1] == "-s":
-   iOS = platform.machine()
-   vrs = platform.version()
-   print(f"\n\n\n[ {time.strftime('%Y-%m-%d %H:%M:%S')} ]: {txt_path} - OpenFile: success")
-   print(f"[ {time.strftime('%Y-%m-%d %H:%M:%S')} ]: readPuafLines: 0xfffffff00789e950")
-   print(f"[ {time.strftime('%Y-%m-%d %H:%M:%S')} ]: Machine: {iOS}")
-   print(f"[ {time.strftime('%Y-%m-%d %H:%M:%S')} ]: kernel_version: {vrs}")
-   while True:
+    # Verificar se há arquivos .json no diretório
+    json_files = [f for f in os.listdir(directory) if f.endswith('.json')]
+    
+    if not json_files:
+        filepath = create_json_file(directory)
+    else:
+        filepath = os.path.join(directory, json_files[0])
+    
+    data = read_json_file(filepath)
+    
+    if data["- (bool)app_state"] == "true":
+        print(f"""\033[92m╭━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+┋| App-Version: {data["app_version"]}
+┋| Time: {data["timestamp"]}
+┋| App-Status: Online <{data["- (bool)app_state"]}
+┋| Host: localhost
+╰━━━━━━━━━━\033[97m[ BOOLEAN ]\033[92m━━━━━━━━━━ """)
+        input("[ + ]: 'press enter to stop dylib injection' > ")
+        print("\033[91m[ + ]: '_0x5e5ad3' / exit status code '1'")
+        print("\033[92m ++ !!! RESTART YOUR DEVICE TO DISABLE !!! ++")
         
-        shutil.copy(plist_path, txt_path)
+        app_choice = data["app_version"]
+        update_sh_file(app_choice, active=False)  # Atualiza o arquivo .sh para o estado desativado
+        data["- (bool)app_state"] = "false"
+    else:
+        app_choice = input("[ d (dopamine) / r (Roothide) ] => ").strip().lower()
+        print("[ + ]: 0x2af2b8_ / Dylib-injection is Successfully\n")
+        print("[ ! ]: Restart your device To Disable...")
+        
+        if app_choice == 'd':
+            update_sh_file('dopamine', active=True)  # Atualiza o arquivo .sh para o estado ativo
+            process = subprocess.Popen(["sh", "dopamine_starter.sh"])
+            data["app_version"] = "dopamine"
+        elif app_choice == 'r':
+            update_sh_file('roothide', active=True)  # Atualiza o arquivo .sh para o estado ativo
+            process = subprocess.Popen(["sh", "roothide_starter.sh"])
+            data["app_version"] = "roothide"
+        
+        data["- (bool)app_state"] = "true"
+    
+    write_json_file(filepath, data)
 
-        with open(info_file, 'a') as file:
-            file.write(f"[ {time.strftime('%Y-%m-%d %H:%M:%S')} ]: {txt_path} :200: open")
+if __name__ == "__main__":
+    main()
 
-        with open(txt_path, 'r') as file:
-            lines = file.readlines()
-
-        with open(read_puaf_file, 'a') as file:
-            file.write(f"[ {time.strftime('%Y-%m-%d %H:%M:%S')} ]: readPuafLines: {len(lines)} - :200:\n\n")
-  
-
-        with open(txt_path + ".old", 'r') as old_file:
-            old_lines = old_file.readlines() if os.path.exists(txt_path + ".old") else []
-
-        if lines != old_lines:
-            changed_line = None
-            for i, (new_line, old_line) in enumerate(zip(lines, old_lines)):
-                if new_line != old_line:
-                    changed_line = i
-                    break
-
-            if changed_line is not None:
-                app_identifier_line = None
-                for i in range(changed_line, 0, -1):
-                    if "<key>appIdentifier</key>" in lines[i]:
-                        app_identifier_line = i + 1
-                        break
-
-                if app_identifier_line is not None:
-                    bundle_id = clean_tag(lines[app_identifier_line].strip())
-                    app_name = find_app(bundle_id)
-                    if app_name:
-                        refresh_app(app_name)
-                        with open(status_puaf_file, 'a') as file:
-                            file.write(f"[ {time.strftime('%Y-%m-%d %H:%M:%S')} ]: :200: {app_name} -r :success:\n\n")
-
-        shutil.copy(txt_path, txt_path + ".old")
-        time.sleep(1)
-else:
-    print("\n\n\n[+] Use: [ -r (readOnly) | -s (start) ]\n       [+]: [ -i (info) ]")
     sys.exit()
